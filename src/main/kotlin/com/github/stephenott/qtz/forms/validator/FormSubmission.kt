2bac729d4355554acbb6aeced88dba5f7553d47e
StephenOTT <stephen@digitalstate.ca>
2019-11-22 06:18:53 -0500

    Add support for Form Validation

--- src/main/kotlin/com/github/stephenott/qtz/forms/validator/FormSubmission.kt
+++ src/main/kotlin/com/github/stephenott/qtz/forms/validator/FormSubmission.kt
@@ -2,85 +2,64 @@ package com.github.stephenott.qtz.forms.validator
 
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.exceptions.HttpClientResponseException
-import io.micronaut.http.hateoas.JsonError
 import io.micronaut.validation.Validated
-import io.reactivex.Completable
-import io.reactivex.Flowable
-import io.reactivex.Observable
 import io.reactivex.Single
-import io.reactivex.rxkotlin.subscribeBy
 
 
 @Controller("/forms")
 open class FormSubmissionController(private val formValidatorServiceClient: FormValidatorServiceClient) : FormSubmissionOperations {
 
+    @Post(value = "/submit")
     override fun submit(submission: Single<FormSubmission>): Single<HttpResponse<FormSubmission>> {
         return submission.map { HttpResponse.ok(it) }
     }
 
+    @Post(value = "/validate")
     override fun validate(submission: Single<FormSubmission>): Single<HttpResponse<ValidationResponseValid>> {
-            return formValidatorServiceClient.validate(submission).map {
-                HttpResponse.ok(it.body()!!)
-            }
+        return formValidatorServiceClient.validate(submission)
+                .onErrorResumeNext {
+                    // @TODO Can eventually be replaced once micronaut-core fixes a issue where the response body is not passed to @Error handler when it catches the HttpClientResponseException
+                    if (it is HttpClientResponseException) {
+                        val body = it.response.getBody(ValidationResponseInvalid::class.java)
+                        if (body.isPresent) {
+                            Single.error(FormValidationException(body.get()))
+                        } else {
+                            Single.error(IllegalStateException("Invalid Response Received", it))
+                        }
+                    } else {
+                        Single.error(IllegalStateException("Unexpected Error received from Form Validation request.", it))
+                    }
+                }.map {
+                    HttpResponse.ok(it.body()!!)
+                }
     }
 
     @Error
-    fun formSubmissionException(request: HttpRequest<*>, exception: HttpClientResponseException): Single<HttpResponse<JsonError>> {
-        println(exception.response)
-        return Single.just(
-                HttpResponse.status<JsonError>(HttpStatus.BAD_REQUEST, "Form Validation Failure")
-                            .body(JsonError(exception.response.body.toString()))
-        )
+    fun formValidationError(request: HttpRequest<*>, exception: FormValidationException): HttpResponse<ValidationResponseInvalid> {
+        return HttpResponse.badRequest(exception.responseBody)
     }
 }
 
-
-//    @Error
-//    fun formSubmissionException(request: HttpRequest<Any>, exception: Throwable): Single<HttpResponse<JsonError>> {
-//        when (exception) {
-//            is HttpClientResponseException -> {
-//                return if (exception.response.status == HttpStatus.valueOf(400)) {
-//                    println(exception.response.body())
-//                    Single.just(HttpResponse.status<JsonError>(HttpStatus.BAD_REQUEST, "Form Validation Failure")
-//                            .body(JsonError(exception.response.body().toString())))
-//                } else {
-//                    //@TODO Add logging
-//                    exception.printStackTrace()
-//                    Single.just(HttpResponse.status<JsonError>(HttpStatus.SERVICE_UNAVAILABLE, "Service Unavailable"))
-//                }
-//            }
-//            else -> {
-//                return Single.just(HttpResponse.status<JsonError>(HttpStatus.SERVICE_UNAVAILABLE, "Service Unavailable"))
-//            }
-//        }
-//    }
-//}
-
 @Validated
 interface FormSubmissionOperations {
-
-    @Post(value = "/submit")
     fun submit(@Body submission: Single<FormSubmission>): Single<HttpResponse<FormSubmission>>
 
-    @Post(value = "/validate")
     fun validate(@Body submission: Single<FormSubmission>): Single<HttpResponse<ValidationResponseValid>>
 }
 
 //@Introspected
 data class FormSubmission(
         val schema: FormSchema,
-        val submission: FormSubmissionObject) {
-}
+        val submission: FormSubmissionData) {}
 
 //@Introspected
-data class FormSubmissionObject(
+data class FormSubmissionData(
         val data: Map<String, Any>,
-        val metadata: Map<String, Any>?
-) {}
+        val metadata: Map<String, Any>?) {}
+
+class FormValidationException(val responseBody: ValidationResponseInvalid) : RuntimeException("Form Validation Exception") {}
--- src/main/kotlin/com/github/stephenott/qtz/forms/validator/ValidatorService.kt
+++ src/main/kotlin/com/github/stephenott/qtz/forms/validator/ValidatorService.kt
@@ -1,16 +1,23 @@
 package com.github.stephenott.qtz.forms.validator
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonUnwrapped
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.DeserializationFeature.*
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.jackson.annotation.JacksonFeatures
 import io.reactivex.Single
 
 
 @Client("\${formValidatorService.host}")
+@JacksonFeatures(
+        enabledDeserializationFeatures = [FAIL_ON_NULL_FOR_PRIMITIVES, FAIL_ON_IGNORED_PROPERTIES]
+)
 interface FormValidatorServiceClient {
 
     @Post("/validate")
@@ -18,28 +25,10 @@ interface FormValidatorServiceClient {
 
 }
 
-
-data class ValidationRequest(val submission: FormSubmission) {}
-
-data class ValidationResponse(val response: ValidationResponseType)
-
-interface ValidationResponseType {
-    val result: ValidationRequestResult
-}
-
-enum class ValidationRequestResult {
-    VALID, INVALID, ERROR
-}
-
 data class ValidationResponseValid(@JsonUnwrapped val processed_submission: Map<String, Any>)
 
-data class ValidationResponseInvalid(val isJoi: Boolean,
+data class ValidationResponseInvalid(@get:JsonProperty("isJoi") @param:JsonProperty("isJoi") val isJoi: Boolean,
                                      val name: String,
                                      val details: List<Map<String, Any>>,
                                      val _object: Map<String, Any>,
-                                     val _validated: Map<String, Any>,
-                                     override val result: ValidationRequestResult = ValidationRequestResult.INVALID) : ValidationResponseType
-
-data class ValidationResponseError(val statusCode: HttpStatus,
-                                   val errorMessage: String,
-                                   override val result: ValidationRequestResult = ValidationRequestResult.ERROR) : ValidationResponseType
+                                     val _validated: Map<String, Any>)
